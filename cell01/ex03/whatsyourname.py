5d3fad741da2497d474bc0c659b72df042899b44
67070046-Natachai <176611935+67070046-Natachai@users.noreply.github.com>
2026-02-11 19:45:47 +0700

    Create whatsyourname.py

---
+++ cell01/ex03/whatsyourname.py
@@ -0,0 +1,7 @@
+"""cell01"""
+def ilove42():
+    """ex03"""
+    first_name = input("Hey, what's your first name? : ")
+    last_name = input("And your last name? : ")
+    print(f"Well, pleased to meet you, {first_name} {last_name}.")
+ilove42()
