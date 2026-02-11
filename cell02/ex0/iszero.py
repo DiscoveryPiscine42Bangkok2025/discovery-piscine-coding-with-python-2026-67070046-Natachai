bbee541098368c1ca7994003694642e918edfcd9
67070046-Natachai <176611935+67070046-Natachai@users.noreply.github.com>
2026-02-11 20:45:44 +0700

    Create iszero.py

---
+++ cell02/ex0/iszero.py
@@ -0,0 +1,9 @@
+"""cell02"""
+def ilove42():
+    """ex0"""
+    num = int(input())
+    if num == 0:
+        print("This number is equal to zero.")
+    else:
+        print("This number is different from zero.")
+ilove42()
