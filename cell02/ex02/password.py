79dbe1ca564de0d51a3398c5471dc1be5555274f
67070046-Natachai <176611935+67070046-Natachai@users.noreply.github.com>
2026-02-11 22:59:40 +0700

    Create password.py

---
+++ cell02/ex02/password.py
@@ -0,0 +1,10 @@
+"""cell02"""
+def ilove42():
+    """ex02"""
+    password = "Python is awesome"
+    user = input("Enter the password: ")
+    if user == password:
+        print("ACCESS GRANTED")
+    else:
+        print("ACCESS DENIED")
+ilove42()
